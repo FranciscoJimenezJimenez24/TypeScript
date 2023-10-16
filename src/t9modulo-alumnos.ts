dcf024d9bead57321746adedb3fd897ccb4704a1
franjimenez <franjimenezjimenez24@gmail.com>
2023-10-16 22:30:24 +0200

    Format code

--- src/t9modulo-alumnos.ts
+++ src/t9modulo-alumnos.ts
@@ -1,88 +1,90 @@
 //Importamos los objetos y la funciones
-import { Direccion,Alumno,Ensenyanza,Instituto,direccionInstituto,
-    matricularAlumnos,mostrarAlumnos } from "./t9modulo-instituto";
+import {
+    Direccion, Alumno, Ensenyanza, Instituto, direccionInstituto,
+    matricularAlumnos, mostrarAlumnos
+} from "./t9modulo-instituto";
 //Creamos los objetos que nos pide el ejercicio
-const ensenyanza1:Ensenyanza={
-    nivel:"Secundaria",
-    obligatoria:true,
-    turno:"Mañana",
-    direccion:{
-        calle:"Avenida de la Libertad, 56",
-        cp:54323,
-        localidad:"Zaragoza",
-        provincia:"Zaragoza"
+const ensenyanza1: Ensenyanza = {
+    nivel: "Secundaria",
+    obligatoria: true,
+    turno: "Mañana",
+    direccion: {
+        calle: "Avenida de la Libertad, 56",
+        cp: 54323,
+        localidad: "Zaragoza",
+        provincia: "Zaragoza"
     }
 }
 
-const ensenyanza2:Ensenyanza={
-    nivel:"Bachillerato",
-    obligatoria:true,
-    turno:"Tarde",
-    direccion:{
-        calle:"Calle del Sol, 67",
-        cp:54321,
-        localidad:"Zaragoza",
-        provincia:"Zaragoza"
-    }
-} 
-
-const ensenyanza3:Ensenyanza={
-    nivel:"Formacion Profesional",
-    obligatoria:false,
-    fp:"Ciclo Formativo de Grado Superior de Desarrollo de Aplicaciones Multiplataforma",
-    turno:"Telemático",
-    direccion:{
-        calle:"Calle de los Sueños, 27",
-        cp:54367,
-        localidad:"Zaragoza",
-        provincia:"Zaragoza"
-    }
-} 
-
-const alumno1:Alumno={
-    nombre:"Juan Pérez",
-    edad:16,
-    sexo:"Masculino",
-    ensenyanza:ensenyanza1,
-    direccion:{
-        calle:"Calle de los Pájaros, 45",
-        cp:54321,
-        localidad:"Zaragoza",
-        provincia:"Zaragoza"
+const ensenyanza2: Ensenyanza = {
+    nivel: "Bachillerato",
+    obligatoria: true,
+    turno: "Tarde",
+    direccion: {
+        calle: "Calle del Sol, 67",
+        cp: 54321,
+        localidad: "Zaragoza",
+        provincia: "Zaragoza"
     }
 }
 
-const alumno2:Alumno={
-    nombre:"María González",
-    edad:17,
-    sexo:"Femenino",
-    ensenyanza:ensenyanza2,
-    direccion:{
-        calle:"Calle del Bosque, 34",
-        cp:54323,
-        localidad:"Zaragoza",
-        provincia:"Zaragoza"
+const ensenyanza3: Ensenyanza = {
+    nivel: "Formacion Profesional",
+    obligatoria: false,
+    fp: "Ciclo Formativo de Grado Superior de Desarrollo de Aplicaciones Multiplataforma",
+    turno: "Telemático",
+    direccion: {
+        calle: "Calle de los Sueños, 27",
+        cp: 54367,
+        localidad: "Zaragoza",
+        provincia: "Zaragoza"
     }
 }
 
-const instituto:Instituto={
-    nombre:"Instituto Educativo San Juan",
-    director:"María Rodríguez",
-    telefono:"123456789",
-    correo:"contacto@institutosanjuan.edu",
-    direccion:{
-        calle:"Avenida Principal, 123",
-        cp:12345,
-        localidad:"Zaragoza",
-        provincia:"Zaragoza"
+const alumno1: Alumno = {
+    nombre: "Juan Pérez",
+    edad: 16,
+    sexo: "Masculino",
+    ensenyanza: ensenyanza1,
+    direccion: {
+        calle: "Calle de los Pájaros, 45",
+        cp: 54321,
+        localidad: "Zaragoza",
+        provincia: "Zaragoza"
+    }
+}
+
+const alumno2: Alumno = {
+    nombre: "María González",
+    edad: 17,
+    sexo: "Femenino",
+    ensenyanza: ensenyanza2,
+    direccion: {
+        calle: "Calle del Bosque, 34",
+        cp: 54323,
+        localidad: "Zaragoza",
+        provincia: "Zaragoza"
+    }
+}
+
+const instituto: Instituto = {
+    nombre: "Instituto Educativo San Juan",
+    director: "María Rodríguez",
+    telefono: "123456789",
+    correo: "contacto@institutosanjuan.edu",
+    direccion: {
+        calle: "Avenida Principal, 123",
+        cp: 12345,
+        localidad: "Zaragoza",
+        provincia: "Zaragoza"
     },
-    alumnos:[]
+    alumnos: []
 }
 //Y utilizamos los metodos
-matricularAlumnos(alumno1,instituto);
-matricularAlumnos(alumno2,instituto);
+matricularAlumnos(alumno1, instituto);
+matricularAlumnos(alumno2, instituto);
 mostrarAlumnos(instituto);
 direccionInstituto(instituto);
 
 
-    
+
--- src/t9modulo-instituto.ts
+++ src/t9modulo-instituto.ts
@@ -1,54 +1,54 @@
 //Exportamos todos los interfaces y funciones
 //Creamos un interface direccion
-export interface Direccion{
-    calle:string,
-    cp:number,
-    localidad:string,
-    provincia:string
+export interface Direccion {
+    calle: string,
+    cp: number,
+    localidad: string,
+    provincia: string
 }
 //Creamos un interface enseñanza, que tiene un objeto direccion
-export interface Ensenyanza{
-    nivel:string,
-    obligatoria:boolean,
-    fp?:string,
-    turno:"Mañana"|"Tarde"|"Telemático"
-    direccion:Direccion
+export interface Ensenyanza {
+    nivel: string,
+    obligatoria: boolean,
+    fp?: string,
+    turno: "Mañana" | "Tarde" | "Telemático"
+    direccion: Direccion
 }
 //Creamos un interface alumno, que tiene un objeto direccion y enseñanza
-export interface Alumno{
-    nombre:string,
-    edad:number,
-    sexo:"Masculino"|"Femenino",
-    ensenyanza:Ensenyanza,
-    direccion:Direccion
+export interface Alumno {
+    nombre: string,
+    edad: number,
+    sexo: "Masculino" | "Femenino",
+    ensenyanza: Ensenyanza,
+    direccion: Direccion
 }
 //Creamos un interface instituto, que tiene un objeto direccion
-export interface Instituto{
-    nombre:string,
-    director:string,
-    telefono:string,
-    correo:string,
-    direccion:Direccion,
-    alumnos:Alumno[]
+export interface Instituto {
+    nombre: string,
+    director: string,
+    telefono: string,
+    correo: string,
+    direccion: Direccion,
+    alumnos: Alumno[]
 }
 //Este metodo matricula un alumno en un instituto
-export function matricularAlumnos(alumno:Alumno,{alumnos}:Instituto){
+export function matricularAlumnos(alumno: Alumno, { alumnos }: Instituto) {
     alumnos.push(alumno)
 }
 //Este metodo muestra el nombre, el nivel y la localidad de los alumnos de un instituto
-export function mostrarAlumnos(instituto:Instituto){
-    const {alumnos}=instituto
-    alumnos.forEach(alumno=>{
-        const {nombre,ensenyanza,direccion}=alumno
-        const {nivel}=ensenyanza
-        const {localidad}=direccion
-        console.log("Nombre:",nombre,
-                    "\nNivel:",nivel,
-                    "\nLocalidad:",localidad)
+export function mostrarAlumnos(instituto: Instituto) {
+    const { alumnos } = instituto
+    alumnos.forEach(alumno => {
+        const { nombre, ensenyanza, direccion } = alumno
+        const { nivel } = ensenyanza
+        const { localidad } = direccion
+        console.log("Nombre:", nombre,
+            "\nNivel:", nivel,
+            "\nLocalidad:", localidad)
     })
 }
 //Este metodo muestra la direccion de un instituto
-export function direccionInstituto({direccion}:Instituto){
-    const {calle,cp,localidad,provincia}=direccion
-    console.log(calle,",",cp,",",localidad,",",provincia)
+export function direccionInstituto({ direccion }: Instituto) {
+    const { calle, cp, localidad, provincia } = direccion
+    console.log(calle, ",", cp, ",", localidad, ",", provincia)
 }
